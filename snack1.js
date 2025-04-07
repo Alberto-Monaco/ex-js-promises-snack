8ba5de63cfcfc0f9f4062c2743c3a67eaf3c595b
Alberto Monaco <alberto.monaco84@gmail.com>
2025-04-07 18:43:28 +0200

    add comment

--- snack1.js
+++ snack1.js
@@ -4,15 +4,24 @@
 // Crea una funzione getPostTitle(id) che accetta un id e
 // restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
 
+// Function that returns a Promise to get a post title by ID
 function getPostTitle(id) {
+	// Create and return a new Promise
 	return new Promise((resolve, reject) => {
+		// Fetch data from the API using the provided ID
 		fetch(`https://dummyjson.com/posts/${id}`)
+			// Convert the response to JSON
 			.then((response) => response.json())
+			// Extract the title from the data and resolve the Promise
 			.then((data) => resolve(data.title))
+			// If any error occurs, reject the Promise with the error
 			.catch((error) => reject(error))
 	})
 }
 
+// Example usage: get the title of post with ID 1
 getPostTitle(1)
+	// Log the title if successful
 	.then((title) => console.log(title))
+	// Log any errors that occur
 	.catch((error) => console.error(error))
--- snack2.js
+++ snack2.js
@@ -4,19 +4,25 @@
 // Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
 
 function lanciaDado() {
+	// Creiamo una nuova Promise
 	return new Promise((resolve, reject) => {
+		// Utilizziamo setTimeout per simulare un'attesa di 3 secondi
 		setTimeout(() => {
+			// Genera un numero casuale tra 1 e 6
 			const numeroCasuale = Math.floor(Math.random() * 6) + 1
 
+			// Nel 20% dei casi (random < 0.2) il dado si incastra
 			if (Math.random() < 0.2) {
 				reject('Il dado si è incastrato!')
 			} else {
+				// Negli altri casi, restituisce il numero generato
 				resolve(numeroCasuale)
 			}
 		}, 3000)
 	})
 }
 
+// Chiamiamo la funzione e gestiamo il risultato
 lanciaDado()
-	.then((res) => console.log('Risultato del lancio:', res))
-	.catch((error) => console.log('Errore:', error))
+	.then((res) => console.log('Risultato del lancio:', res)) // In caso di successo
+	.catch((error) => console.log('Errore:', error)) // In caso di errore
